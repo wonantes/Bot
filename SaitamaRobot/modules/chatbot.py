76de8d3cf7d34149c1403872a6e914a6893c77cd
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2020-12-30 10:45:43 +0530

    Update chatbot.py

--- SaitamaRobot/modules/chatbot.py
+++ SaitamaRobot/modules/chatbot.py
@@ -136,7 +136,7 @@ def list_chatbot_chats(update: Update, context: CallbackContext):
 
 
 __help__ = f"""
-Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a more interactive group chat experience.
+Chatbot utilizes the [CoffeeHouse API](https://coffeehouse.intellivoid.net/) and allows Saitama to talk and provides a more interactive group chat experience.
 
 *Commands:* 
 *Admins only:*
@@ -144,7 +144,7 @@ Chatbot utilizes the CoffeeHouse API and allows Saitama to talk and provides a m
  • `/rmchat`*:* Disables Chatbot mode in the chat.
 
 Reports bugs at @{SUPPORT_CHAT}
-*Powered by CoffeeHouse* (https://coffeehouse.intellivoid.net/) from @Intellivoid
+*Powered by* [CoffeeHouse from Intellivoid](https://coffeehouse.intellivoid.net/) from @Intellivoid
 """
 
 ADD_CHAT_HANDLER = CommandHandler("addchat", add_chat)
